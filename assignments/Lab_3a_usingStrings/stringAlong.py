286ec004e8439588ca6083470023285892bdc2d7
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-06 20:00:45 -0700

    pushing update with one possible idea

--- assignments/Lab_3a_usingStrings/stringAlong.py
+++ assignments/Lab_3a_usingStrings/stringAlong.py
@@ -1,1 +1,16 @@
-#
+"""
+    Project Name: IT121_Python
+    Sub-project: Labs
+    File Name: stringAlong.py
+    Author: Dustin McClure
+    Lab: Lab 3a - Using Strings
+    Modified Date: 10/06/2020
+
+    {Description of program here}
+"""
+# Import the class 'Fuck' from the library foaas
+from foaas import fuck
+
+fuck.linus(name='Bill', from_='Dustin')
+
+print(fuck.linus(name='Bill', from_='Dustin').url)
