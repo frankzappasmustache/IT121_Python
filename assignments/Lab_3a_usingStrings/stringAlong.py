e1bec4d2fbca027062710883c08769cabc9ffabb
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-09 13:25:24 -0700

    pushing most recent udpates

--- assignments/Lab_3a_usingStrings/stringAlong.py
+++ assignments/Lab_3a_usingStrings/stringAlong.py
@@ -10,11 +10,30 @@
 """
 # Import the class 'Fuck' from the library foaas
 from foaas import Fuck
+from io import StringIO
+import json
+from bs4 import BeautifulSoup
+import requests
+import re
 
-ops = fuck.operations().json
-
-print(ops)
-
-linusRant = fuck.linus(name='Bill', from_='Dustin')
+fuck = Fuck()
 
 
+def magicalFuck():
+    print('''Find the most effective application
+to tell whoever or whatever to 'fuck off' by
+choosing from the following comprehensive list
+of actions.\n''')
+
+    print('Main Menu: ')
+    for count, item in enumerate(fuck.actions, 0):
+        fuckValues = ''.join(str(item))
+        print(count, fuckValues)
+    print('\n')
+    fuckChoice = input('''Choose an Option
+    From the Fucks Listed: [0-92] : ''')
+    fuckChoice = int(fuckChoice)
+    fuckingRequest = input("Please enter fuck options: " )
+    return requests.post(fuckingRequest)
+
+magicalFuck()
