46e3439c9187cf6f031207f89b7e25a7e62dbdec
frankzappasmustache <dustin.t.mcclure@gmail.com>
2020-10-21 17:58:54 -0700

    pushing working phonebook lookup...finally

--- assignments/Lab_3b_Phone_Book_Look_up /phoneBookLookUp.py
+++ assignments/Lab_3b_Phone_Book_Look_up /phoneBookLookUp.py
@@ -1,40 +1,70 @@
 """
-Python Program that asks for name/phone number pairs, adds them to a map, and then provides a lookup function for the user to look this info up.
+    Project Name: IT121_Python
+    Sub-project: Labs
+    File Name: fuckstrings.py
+    Author: Dustin McClure
+    Lab: Lab 3a - Using Strings
+    Modified 1: Date0/10/2020
+
+    Python Program that asks for name/phone number pairs,
+    adds them to a map, and then provides a lookup function for the user to
+    look this info up.
 """
-phone_book = [];
 
-def get_contact_name(contactName):
-    contactName = input("Enter a new contact name: ")
-    phone_book.append(contactName)
+phone_book = {}
 
-def get_contact_phone(contactPhone):
-    contactPhone = input("Enter a new phone number for this contact: ")
-    phone_book.append(contactPhone)
+def add_contact():
+    name = input("Please enter a contact name for this new contact: ")
+    email = input("Please enter an email address for this new contact: ")
+    phone = input("Please enter a phone number for this new contact: ")
+    print('''This is the information for the new contact you added
+        Full Name: %s
+        Email: %s
+        Phone Number: %s
+        ''' % (name, email, phone))
+    name2 = input("Please enter a contact name for this new contact: ")
+    email2 = input("Please enter an email address for this new contact: ")
+    phone2 = input("Please enter a phone number for this new contact: ")
+    print('''This is the information for the new contact you added
+        Full Name: %s
+        Email: %s
+        Phone Number: %s
+        ''' % (name2, email2, phone2))
+    name3 = input("Please enter a contact name for this new contact: ")
+    email3 = input("Please enter an email address for this new contact: ")
+    phone3 = input("Please enter a phone number for this new contact: ")
+    print('''This is the information for the new contact you added
+        Full Name: %s
+        Email: %s
+        Phone Number: %s
+        ''' % (name3, email3, phone3))
 
-    def add_email(name, email):
-    add_contact(name)
-    if 'emails' in phone_book[name]:
-        if email in phone_book[name]['emails']:
-            pass
-        else:
-            phone_book[name].append(email)
-    else:
-        phone_book[name]['emails'] = [email]
-    print("email added")
+    phone_book1 = {
+    0: [
+    name,
+    email,
+    phone
+    ],
+    1: [
+    name2,
+    email2,
+    phone2
+    ],
+    2: [
+    name3,
+    email3,
+    phone3
+    ]
+    }
 
-    def search_name(name):
-    if name in phone_book:
-        print(phone_book[name])
-    else:
-        print("name didn't exist")
+    phone_book.update(phone_book1)
 
-def search_number(number):
-    for name in phone_book:
-        if 'numbers' in phone_book[name]:
-            if number in phone_book[name]['numbers']:
-                print(name)
-                return
-    print('number did not exist')
+def find_name_number_email(data):
+    search = input("Please enter the name, email, or phone number you would like to look up: ")
+    values = [value for value in phone_book.values() if search in value]
+    print(values)
 
-contactPhone1 = input("Enter a new contact phone number: ");
-get_contact_phone(contactPhone1)
+
+add_contact()
+print(phone_book)
+find_name_number_email(data=phone_book)
